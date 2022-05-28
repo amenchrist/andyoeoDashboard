b85d6aed811f81fd9b8659fecf1484d281a78277
Amen Christ <amenphronesis@gmail.com>
2022-05-28 14:00:50 +0100

    Added search function

--- src/pages/AllApplications.js
+++ src/pages/AllApplications.js
@@ -6,13 +6,16 @@ const AllApplications = () => {
 
   const [ searchQuery , setSearchQuery ] = useState('');
   const [ tableData, setTableData ] = useState([]);
+  const [ initialTableData, setInitialTableData ] = useState([]);
 
     useEffect(()=>{
         fetch('http://localhost:7000/applications')
         .then(res => res.json())
         .then(data => {
           //console.log(data);
-          setTableData(data)})
+          setTableData(data);
+          setInitialTableData(data);})
+          
         try{
         }catch {
             console.log('Problem fetching Data');
@@ -23,15 +26,24 @@ const AllApplications = () => {
 
   function handleSearch (e) {
     setSearchQuery(e.target.value);
-    console.log(searchQuery)
+    let query = e.target.value.trim();
+    if(query !== ''){
+      const reg = new RegExp(query, 'i')
+      const results = initialTableData.filter(data =>{
+        return data.applicant.match(reg);
+      })
+      setTableData(results);
+    } else {
+      setTableData(initialTableData);
+    }
   }
 
-
+  
 
   return (
     <div  className='pt5 pb5'>
         <div className='pb2'>
-            <TextField value={searchQuery} onChange={handleSearch} placeholder='Search'/>
+            <TextField value={searchQuery} onChange={handleSearch} placeholder='Search Applicants'/>
         </div>
         <div className='tc'>
             <StickyHeadTable data={tableData} />
